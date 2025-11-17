5931746ddb51347f891d850490615cc8a19f54ff
antiquebooks <martin.macuha@gmail.com>
2025-11-17 14:45:35 +0100

    Configure static export and trailing slash options
    Added output and trailingSlash options to nextConfig.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true
-  }
+  },
+  output: 'export',     // make Next produce a static export
+  trailingSlash: true   // export routes as /path/index.html (better for GitHub Pages)
 }
 
 module.exports = nextConfig
